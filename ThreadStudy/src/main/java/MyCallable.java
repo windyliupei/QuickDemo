46cea8251ee98c2db6aa10f32e3e4bc86d8ef8bb
windy <windyliupei@126.com>
2021-04-27 14:55:12 +0800

    added callable code

--- ThreadStudy/src/main/java/MyCallable.java
+++ ThreadStudy/src/main/java/MyCallable.java
@@ -1,9 +1,15 @@
+import java.util.Date;
 import java.util.concurrent.Callable;
 
-public class MyCallable implements Callable {
+public class MyCallable implements Callable<String> {
 
     @Override
-    public Object call() throws Exception {
-        return null;
+    public String call() throws Exception {
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(Thread.currentThread().getName()+": 执行时间是"+new Date().getTime()+":"+"循环次数："+i);
+        }
+
+        return "MyCallable Return";
     }
 }
--- ThreadStudy/src/main/java/ThreadCreateDemo.java
+++ ThreadStudy/src/main/java/ThreadCreateDemo.java
@@ -1,24 +1,52 @@
 import java.util.Date;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 public class ThreadCreateDemo {
 
     public static void main(String[] args){
 
         //继承 Thread 类去创建线程。
-        myThread();
+        //myThread();
 
         //实现 Runnable 去创建线程。
-        runnable();
+        //runnable();
 
         //实现 Callable 去创建线程。
         //这个需要借助 Future 类
+        myCallable();
 
+    }
 
+    private static void myCallable() {
+        MyCallable myCallable = new MyCallable();
+        FutureTask<String> stringFutureTask = new FutureTask<String>(myCallable);
+        Thread t1 = new Thread(stringFutureTask,"Mycallable");
+        t1.start();
+        try {
+            String callableResult = stringFutureTask.get(1000, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+        }
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Main 主线程"+new Date().getTime());
+        }
     }
 
     private static void runnable() {
         Thread thread = new Thread(new MyRunnable(),"MyRunnableThread");
         thread.start();
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Main 主线程"+new Date().getTime());
+        }
     }
 
     private static void myThread() {
